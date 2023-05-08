c2cf86506711abe3ba8178ea94550e254e78d03c
Mariia <mariia.khomenko20gmail.com>
2023-05-08 18:52:28 +0300

    creating board management interface

--- app/app.php
+++ app/app.php
@@ -1,5 +1,23 @@
 <?php
-$board = emptyBorder();
+
+session_start();
+
+if (!empty($_SESSION["board"]))
+    $board = $_SESSION["board"];
+else
+    $board = emptyBorder();
+
+if (!empty($_SESSION["queensCount"]))
+    $queensCount = $_SESSION["queensCount"];
+else
+    $queensCount = 8;
+
+function saveValues()
+{
+    global $board, $queensCount;
+    $_SESSION["board"] = $board;
+    $_SESSION["queensCount"] = $queensCount;
+}
 
 function alert($message)
 {
@@ -11,9 +29,88 @@ function emptyBorder()
     return array_fill(0, 8, array_fill(0, 8, 0));
 }
 
-function clearBoard()
+function validateCellIndexes($row, $column)
 {
     global $board;
+    if (isset($row) && isset($column)) {
+        if (is_numeric($row) && is_numeric($column) && $row < count($board) && $column < count($board)) {
+            return true;
+        } else
+            alert("Enter the correct values!");
+    } else
+        alert("Enter all values!");
+    return false;
+}
+
+function clearBoard()
+{
+    global $board, $queensCount;
     $board = emptyBorder();
+    $queensCount = 8;
+    saveValues();
+}
+
+function setQueen($row, $column)
+{
+    global $board, $queensCount;
+    if ($board[$row][$column] != 0)
+        alert("It is forbidden to put a queen in this cell!");
+    else {
+        $n = count($board);
+        for ($c = 0; $c < $n; $c++) {
+            // horizontal and vertical
+            $board[$row][$c]++;
+            $board[$c][$column]++;
+
+            // diagonals
+            $rp = $row + $c;
+            $rm = $row - $c;
+            $cp = $column + $c;
+            $cm = $column - $c;
+
+            if ($rp < $n && $cp < $n)
+                $board[$rp][$cp]++;
+            if ($rm >= 0 && $cm >= 0)
+                $board[$rm][$cm]++;
+            if ($rp < $n && $cm >= 0)
+                $board[$rp][$cm]++;
+            if ($rm >= 0 && $cp < $n)
+                $board[$rm][$cp]++;
+        }
+        $board[$row][$column] = -1;
+        $queensCount--;
+        saveValues();
+    }
+}
+
+function removeQueen($row, $column)
+{
+    global $board, $queensCount;
+    if ($board[$row][$column] === -1) {
+        $n = count($board);
+        for ($c = 0; $c < $n; $c++) {
+            // horizontal and vertical
+            $board[$row][$c]--;
+            $board[$c][$column]--;
+
+            // diagonals
+            $rp = $row + $c;
+            $rm = $row - $c;
+            $cp = $column + $c;
+            $cm = $column - $c;
+
+            if ($rp < $n && $cp < $n)
+                $board[$rp][$cp]--;
+            if ($rm >= 0 && $cm >= 0)
+                $board[$rm][$cm]--;
+            if ($rp < $n && $cm >= 0)
+                $board[$rp][$cm]--;
+            if ($rm >= 0 && $cp < $n)
+                $board[$rm][$cp]--;
+        }
+        $board[$row][$column] = 0;
+        $queensCount++;
+        saveValues();
+    }
 }
 ?>
--- app/index.php
+++ app/index.php
@@ -1,7 +1,27 @@
 <?php
 include "app.php";
 
-$board[4][2] = 1;
+if (isset($_POST['set_queen'])) {
+    $row = $_POST["row"];
+    $column = $_POST["column"];
+    if (validateCellIndexes($row, $column))
+        setQueen($row, $column);
+}
+
+if (isset($_POST['remove_queen'])) {
+    $row = $_POST["row"];
+    $column = $_POST["column"];
+    if (validateCellIndexes($row, $column))
+        removeQueen($row, $column);
+}
+
+if (isset($_POST['clear_board'])) {
+    clearBoard();
+}
+
+if (isset($_POST['start'])) {
+
+}
 ?>
 
 
@@ -19,7 +39,29 @@ $board[4][2] = 1;
 
 <body>
     <div class="flex w-screen h-screen justify-center py-10">
-        <div class="flex flex-col space-y-5">
+        <div class="flex flex-col space-y-10">
+            <form method="post">
+                <div class="flex flex-col space-y-5">
+                    <div class="flex flex-row space-x-3 items-end">
+                        <div class="flex flex-col space-y-1">
+                            <label class="text-sm">Row</label>
+                            <input name="row" maxlength="1"
+                                class="w-24 rounded-sm py-1 px-3 outline outline-1 focus:outline-2">
+                        </div>
+                        <div class="flex flex-col space-y-1">
+                            <label class="text-sm">Column</label>
+                            <input name="column" maxlength="1"
+                                class="w-24 rounded-sm py-1 px-3 outline outline-1 focus:outline-2">
+                        </div>
+                        <button name="set_queen"
+                            class="rounded-sm h-12 px-10 text-white bg-blue-800 hover:bg-blue-700">Set Queen</button>
+                        <button name="remove_queen"
+                            class="rounded-sm h-12 px-10 text-white bg-red-800 hover:bg-red-700">Remove Queen</button>
+                        <button name="clear_board"
+                            class="rounded-sm h-12 px-10 text-white bg-red-800 hover:bg-red-700">Clear Board</button>
+                    </div>
+                </div>
+            </form>
             <div class="flex flex-row">
                 <table id="board">
                     <thead>
@@ -45,8 +87,10 @@ $board[4][2] = 1;
                                 else
                                     $color = "bg-slate-200 text-gray-800";
                                 echo "<td><div class='flex items-center justify-center w-14 h-14 p-1 fill-current $color'>";
-                                if ($board[$i][$j] === 1)
+                                if ($board[$i][$j] === -1)
                                     echo file_get_contents("./assets/queen.svg");
+                                else if ($board[$i][$j] > 0)
+                                    echo $board[$i][$j];
                                 echo "</div></td>";
                             }
                             echo "</tr>";
